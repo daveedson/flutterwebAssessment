2378f4843eac687b92e8ca50604cbb73c2bf7101
David Onoh <davidonoh@gmail.com>
2024-07-19 05:28:52 +0100

    Added test cases for auth repository

--- test/features/authentication/data/auth_repository_test.dart
+++ test/features/authentication/data/auth_repository_test.dart
@@ -1,7 +1,53 @@
+
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:new_web_assesment/features/authentication/data/auth_repository.dart';
+import 'package:new_web_assesment/features/authentication/domain/app_user.dart';
 
 void main() {
-  testWidgets('auth repository ...', (tester) async {
-    // TODO: Implement test
+  const email = "davidonoh@gmail.com";
+  const password = "Password1!";
+  final testUser = AppUser(uid: email.split('').reversed.join(), email: email);
+  group("AuthRepository Test", () {
+    test("test that the current user is null upon login", () {
+      final authRepository = AuthRepository();
+      expect(authRepository.currentUser, null);
+      expect(authRepository.authStateChanges(), emits(null));
+    });
+  });
+
+  test("test that signInwithemailandPassword works", () async {
+    final authRepository = AuthRepository();
+
+    await authRepository.signInWithEmailAndPassword(email, password);
+    expect(authRepository.currentUser, testUser);
+    expect(authRepository.authStateChanges(), emits(testUser));
+  });
+
+  test("test that CreatewithemailandPassword works  ", () async {
+    final authRepository =AuthRepository();
+
+    await authRepository.createUserWithEmailAndPassword(email, password);
+    expect(authRepository.currentUser, testUser);
+    expect(authRepository.authStateChanges(), emits(testUser));
+  });
+
+  test("test that current user is null after signOut", () async {
+    final authRepository =AuthRepository();
+    await authRepository.signInWithEmailAndPassword(email, password);
+    expect(
+      authRepository.authStateChanges(),
+      emitsInOrder([testUser, null]),
+    );
+    await authRepository.signOut();
+
+    expect(authRepository.currentUser, null);
+  });
+
+  test("test that signin after dispose throws exception", () async {
+    final authRepository = AuthRepository();
+    authRepository.dispose();
+    expect(() => authRepository.signInWithEmailAndPassword(email, password),
+        throwsStateError);
   });
 }
